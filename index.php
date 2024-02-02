52c64a72624687401a7e730e1781d5f7a7293286
mikwbm <mikwbm@gmail.com>
2024-02-02 06:52:14 -0800

    yes

--- index.php
+++ index.php
@@ -56,17 +56,10 @@
     <div class="container">
         <h1>Welcome to Pacific Roleplay</h1>
         <p>Explore our community and start your roleplaying journey with us.</p>
-        <form action="coming_soon.php" method="post">
-            <input type="text" name="key" placeholder="Enter your key">
-            <input type="submit" value="Submit">
+        <form action="admin_panel.php" method="post">
+            <input type="text" name="admin_key" placeholder="Enter admin key">
+            <input type="submit" value="Login">
         </form>
-        <a href="#" id="admin-link">Admin Login</a>
-        <div class="admin-login" id="admin-login">
-            <form action="admin_login.php" method="post">
-                <input type="text" name="admin_key" placeholder="Enter admin key">
-                <input type="submit" value="Login">
-            </form>
-        </div>
     </div>
 
     <script>
